e70c0c3014b4f4cfa999eb9f16b17b08c1da9567
Sean <shawn.casaus@gmail.com>
2018-11-17 21:21:11 -0700

    connected components

---
+++ Salu/WordCell.swift
@@ -0,0 +1,15 @@
+//
+//  WordCell.swift
+//  Salu
+//
+//  Created by Sean Casaus on 11/17/18.
+//  Copyright © 2018 Sean Casaus. All rights reserved.
+//
+
+import UIKit
+
+class WordCell: UITableViewCell {
+    @IBOutlet weak var wordLabel: UILabel!
+    
+    
+}
