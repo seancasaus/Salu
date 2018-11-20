eaddb6f68a69009a58b45d2324e2b9a8ac946a7f
Sean <shawn.casaus@gmail.com>
2018-11-19 19:00:49 -0700

    implemented multi language support

--- Salu/AddWordController.swift
+++ Salu/AddWordController.swift
@@ -8,16 +8,40 @@
 
 import UIKit
 
-class AddWordController: UIViewController {
+class AddWordController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     @IBOutlet weak var wordInput: UITextField!
+    @IBOutlet weak var selectTranslateLanguage: UIPickerView!
     var words = WordModel()
     var fStore = FStore()
-    
+    var languages = ["Spanish", "German", "French", "Portuguese", "Italian"]
+    var currentRow: Int = 0
+    var languagesAbv = ["es", "de", "fr", "pt", "it"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return languages.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return languages[row]
+    }
+    
+    // Capture the picker view selection
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // This method is triggered whenever the user makes a change to the picker selection.
+        // The parameter named row and component represents what was selected.
+        currentRow = row
+    }
+
     @IBAction func AddWord(_ sender: Any) {
-        fStore.translateWord(defaultWord: wordInput.text!, targetLanguage: "fr")
+        let targetLn: String = languagesAbv[currentRow]
+        fStore.translateWord(defaultWord: wordInput.text!, targetLanguage: targetLn)
     }
 }
